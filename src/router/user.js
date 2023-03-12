2d265cd2efc78286e7aa6cc4ee9d5ce057735470
razor999920 <raza9999ali@gmail.com>
2023-03-12 14:04:29 -0600

    New table & db util

--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
